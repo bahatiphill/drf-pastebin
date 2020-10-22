00d1ba7fdf9431d7d2082fe508c8b8eb6a86e45d
philbert <philbert@digitalumuganda.com>
2020-10-22 22:20:46 +0200

    APIView class based views

--- snippets/views.py
+++ snippets/views.py
@@ -5,15 +5,70 @@
 #from snippets.models import Snippet
 #from snippets.serializers import SnippetSerializer
 
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+#from rest_framework import status
+#from rest_framework.decorators import api_view
+#from rest_framework.response import Response
+#from snippets.models import Snippet
+#from snippets.serializers import SnippetSerializer
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 
+class SnippetList(APIView):
+    """
+    List all snippets, or create a new snippet
+    """
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
+    """
+    Retrieve, Update or delete a snippet instance
+    """
+
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet =  self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet =  self.get_object(pk)
+        serializer = SnippetDetail(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+'''
 @api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
     """
@@ -57,7 +112,7 @@ def snippet_detail(request, pk, format=None):
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
+'''
 
 '''
 @csrf_exempt
@@ -78,9 +133,8 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
-'''
 
-'''
+
 @csrf_exempt
 def snippet_detail(request, pk):
     """
